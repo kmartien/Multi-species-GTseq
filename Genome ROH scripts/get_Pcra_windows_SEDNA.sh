e3f1e76c93058028ac184cb5c5bf1244d0290c82
kmartien <115281269+kmartien@users.noreply.github.com>
2025-04-22 09:20:19 -0700

    A few files that got missed.

--- Genome ROH scripts/get_Pcra_windows_SEDNA.sh
+++ Genome ROH scripts/get_Pcra_windows_SEDNA.sh
@@ -13,16 +13,16 @@
 #SBATCH -c 10    # <number of cores to ask for>
 #SBATCH --mem=20G   # job memory request increase to 32G after testing
 #SBATCH -t 24:00:00   # walltime in mins or mins:secs or hrs:mins:secs. Increase to 48hr after testing
-# SBATCH -D /scratch/kmartien/temp		# <folder to change to when starting the job>
+#SBATCH -D /scratch/kmartien/temp		# <folder to change to when starting the job>
 
 # not used:
 # SBATCH -p himem  # this would request the hi-memory nodes, if >96G memory needed.
 # SBATCH --tasks-per-node=8
 ################
 # example usage:
-# cd ../home/khernandez/Genomes
+# cd ../home/kmartien/data
 # SCRIPT=~/scripts/misc/get_Pcra_windows_SEDNA.sh
-# FASTA=GCF_039906515.1_mPseCra1.hap1_genomic.fna
+# FASTA=~/data/GCF_039906515.1_mPseCra1.hap1_genomic.fasta.masked
 # sbatch ${SCRIPT} ${FASTA}
 ################
 module load bio/bedtools/2.29.2 
